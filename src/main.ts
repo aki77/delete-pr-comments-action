10d3059e3fc5b2bdfbb6bf623e6ca668a61cafd3
aki <lala.akira@gmail.com>
2022-09-06 18:23:06 +0900

    First commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,34 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const issueNumber = github.context.issue.number
+    if (!issueNumber) {
+      throw new Error('Could not get pull request number from context, exiting')
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const token = core.getInput('token', {required: true})
+    const bodyContains = core.getInput('bodyContains', {required: true})
 
-    core.setOutput('time', new Date().toTimeString())
+    const octokit = github.getOctokit(token)
+    const response = await octokit.rest.issues.listComments({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: issueNumber
+    })
+
+    const comments = response.data.filter(comment => {
+      return comment.body?.includes(bodyContains)
+    })
+
+    for (const comment of comments) {
+      await octokit.rest.issues.deleteComment({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        comment_id: comment.id
+      })
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
